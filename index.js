de5adff311d3ea68f6a6a38d0a48917318f421d7
Vanskater362 <wag12011@byui.edu>
2020-05-19 00:23:38 -0500

    added node server, and connection to database

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', function (req, res) {
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+    host: "localhost",
+    user: "admin",
+    password: "Xtend321"
+});
+  })
+  .get('/index', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
