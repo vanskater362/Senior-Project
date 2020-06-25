1f5ad15a0a00f659d3e0193e1c43c5a6e2491235
Vanskater362 <wag12011@byui.edu>
2020-06-24 22:37:17 -0500

    miles tracker changes

--- Organizd/src/environments/environment.ts
+++ Organizd/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:5000',
-  apiUrl: 'https://tony.devaxissoftware.com/senior-project',
+  apiUrl: 'http://localhost:5000',
+  //apiUrl: 'https://tony.devaxissoftware.com/senior-project',
   expenseImgUrl: ''
 };
 
